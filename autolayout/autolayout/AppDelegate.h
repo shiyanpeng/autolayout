fd2bdf2290403c624800d8e86005636f2b7efd90
shiyanpeng <63414472@qq.com>
2015-02-25 18:13:20 +0800

    Initial commit

---
+++ autolayout/autolayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  autolayout
+//
+//  Created by syp on 15/2/25.
+//  Copyright (c) 2015年 syp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ autolayout/autolayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  autolayout
+//
+//  Created by syp on 15/2/25.
+//  Copyright (c) 2015年 syp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
